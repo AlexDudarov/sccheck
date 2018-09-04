bbdfc9b8017f6657101f3370f65a4b8db2a56185
DUDARKOU <aliaksandr.dudarkou@softclub.by>
2018-09-04 17:26:29 +0300

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ProfileTableComponent } from './components/profile-table/profile-table.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import {RouterModule, Routes} from '@angular/router';
+import {SearchComponent} from './components/search/search.component';
+
+
+export const routeConfig: Routes = [
+  {
+    path: 'home',
+    component: SearchComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ProfileTableComponent,
+    NavigationComponent,
+    SearchComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(routeConfig),
   ],
+  schemas: [ NO_ERRORS_SCHEMA ],
   providers: [],
   bootstrap: [AppComponent]
 })
